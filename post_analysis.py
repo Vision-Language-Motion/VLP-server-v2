27d90d2a4f14aabcb2a523bb71019180ff8e24ef
Daniel Busch <dakopen185@gmail.com>
2024-07-05 13:48:39 +0200

    updated filepaths

--- post_analysis.py
+++ post_analysis.py
@@ -47,16 +47,29 @@ def get_table_names():
             cursor.close()
             connection.close()
 
-def insert_csv_file_in_db(filepath="./output/predictions.csv"):
+def insert_csv_file_in_db(filepath=None):
     try:
-        with open(filepath, mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                video_id = row['Video Name']
-                start_time = float(row['Start Time (s)'])
-                end_time = float(row['End Time (s)'])
-                prediction = row['Classification']
-                insert_prediction_to_db(video_id, start_time, end_time, prediction)
+        if not filepath:
+            base_output_dir = os.path.join(os.getcwd(), 'output')
+
+            with open(os.path.join(base_output_dir, 'predictions.csv'), mode='r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                    video_id = row['Video Name']
+                    start_time = float(row['Start Time (s)'])
+                    end_time = float(row['End Time (s)'])
+                    prediction = row['Classification']
+                    insert_prediction_to_db(video_id, start_time, end_time, prediction)
+
+        else:
+            with open(filepath, mode='r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                    video_id = row['Video Name']
+                    start_time = float(row['Start Time (s)'])
+                    end_time = float(row['End Time (s)'])
+                    prediction = row['Classification']
+                    insert_prediction_to_db(video_id, start_time, end_time, prediction)
 
     except (Exception, psycopg2.Error) as error:
         print(f"Error while connecting to PostgreSQL or the file does not exist: {error}")
